a03c2c846d4f9b49421e3f45770106394210cc6d
weeecap <dzmitry.mikhasko@gmail.com>
2025-10-06 13:59:37 +0300

    adding ui and refactoring

--- main.py
+++ main.py
@@ -1,95 +1,16 @@
-import math
-import pandas as pd
-import tkinter as tk
+import sys
+from PyQt5.QtWidgets import QApplication
 
-from typing import List, Dict, Tuple 
+from gui import ExcelProcessorApp
 
-class NomenclaturalStreetIndexer:
-    def __init__(self, square_size: int = 500):
+def main():
+    app = QApplication(sys.argv)
+    app.setStyle('Fusion') 
 
-        self.square_size = square_size
-        self.origin_x = None
-        self.origin_y = None
-        
-        self.letters = [chr(i) for i in range(1040, 1072)]  # А-Я
-        self.letters = [letter for letter in self.letters if letter != 'Ё' and letter != 'Й']
-        
-    def set_origin(self, x: float, y: float):
-        
-        self.origin_x = x
-        self.origin_y = y
-        print(f"Начало координат установлено: X={x}, Y={y}")
-
-    def _calculate_(self, x: float, y: float) -> tuple:
-
-        if self.origin_x is None or self.origin_y is None:
-            raise ValueError("Начало координат не установлено!")
-        
-        delta_x = self.origin_x - x 
-        delta_y = y - self.origin_y
-        
-        col_index = math.floor(delta_x / self.square_size)
-        row_index = math.floor(delta_y / self.square_size)
-        
-        return delta_x, delta_y, col_index, row_index
+    window = ExcelProcessorApp()
+    window.show()
     
-    def calculate_nomenclatural_index(self, x: float, y: float) -> str:
-
-        delta_x, delta_y, col_index, row_index = self._calculate_(x,y)
-
-        col_index = math.floor(delta_x / self.square_size)
-        row_index = math.floor(delta_y / self.square_size)
-
-        letter = self.letters[col_index % len(self.letters)]
-        number = row_index + 1  
-        
-        return f"{letter}-{number}"
-    
-    def format_street_name(self, street_name:str) -> str:
-
-        parts = street_name.strip().split()
-        street_type = parts[0]
-        name_only = ' '.join(parts[1:])  
-        adjectives = ['ая', 'ый', 'ой', 'ий', 'ого', 'его', 'ему', 'ому', 'им', 'ым', 'ем', 'ом', 'яя', 'ей', 'ую', 'юю', 'ое', 'ее', 'омо', 'ег']
-
-        if name_only[-2:] in adjectives:
-            return f"{name_only} {street_type}"
-        else:
-            return f"{name_only}, {street_type}"
-
-    
-    def calculate_list_number(self, x: float, y: float, border: int = 4500) -> str:
-
-        delta_x, delta_y, col_index, row_index = self._calculate_(x,y)
-        number = row_index + 1
-        
-        if number <=9:
-            list_number = "Лист 1" if delta_x <= border else "Лист 3"
-        else:
-            list_number = "Лист 2" if delta_x <= border else "Лист 4"
-
-        return list_number
-
-    def export_to_excel(self) -> str:
-        return 'Файл был сохранен'          
-
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
-    indexer = NomenclaturalStreetIndexer(500)
-
-    print('Введите начало координат')
-    x_origin, y_origin = int(input()), int(input())
-
-    indexer.set_origin(x_origin, y_origin)
-    
-    test= 'ул Якуба Коласа'
-    x = 5943402.306
-    y = 5421679.414
-    format_street = indexer.format_street_name(test) 
-    nomenclatural_index = indexer.calculate_nomenclatural_index(x, y)
-    list_number = indexer.calculate_list_number(x, y)
-    
-    print(f"Координаты: X={x}, Y={y}")
-    print(f"Номенклатурный индекс: {nomenclatural_index}")
-    print(f"Номер листа: {list_number}")
-    print(f'Форматированное название: {format_street}')
+    main()
---
+++ nomenclatural.py
@@ -0,0 +1,64 @@
+import math
+from typing import Tuple
+
+class NomenclaturalStreetIndexer:
+    def __init__(self, square_size: int = 500):
+        self.square_size = square_size
+        self.origin_x = None
+        self.origin_y = None
+        
+        self.letters = [chr(i) for i in range(1040, 1072)]
+        self.letters = [letter for letter in self.letters if letter != 'Ё' and letter != 'Й']
+        
+    def set_origin(self, x: float, y: float) -> None:
+        self.origin_x = x
+        self.origin_y = y
+
+    def _calculate_indices(self, x: float, y: float) -> Tuple[float, float, int, int]:
+        if self.origin_x is None or self.origin_y is None:
+            raise ValueError("Начало координат не установлено!")
+        
+        delta_x = self.origin_x - x 
+        delta_y = y - self.origin_y
+        
+        col_index = math.floor(delta_x / self.square_size)
+        row_index = math.floor(delta_y / self.square_size)
+        
+        return delta_x, delta_y, col_index, row_index
+    
+    def calculate_nomenclatural_index(self, x: float, y: float) -> str:
+
+        delta_x, delta_y, col_index, row_index = self._calculate_indices(x, y)
+        
+        letter = self.letters[col_index % len(self.letters)]
+        number = row_index + 1
+        
+        return f"{letter}-{number}"
+    
+    def calculate_list_number(self, x: float, y: float, border: int = 4500) -> str:
+        delta_x, delta_y, col_index, row_index = self._calculate_indices(x, y)
+        number = row_index + 1
+        
+        if number <= 9:
+            list_number = "Лист 1" if delta_x <= border else "Лист 3"
+        else:
+            list_number = "Лист 2" if delta_x <= border else "Лист 4"
+
+        return list_number
+    
+    def format_street_name(self, street_name: str) -> str:
+        parts = street_name.strip().split()
+        
+        if len(parts) < 2:
+            return street_name
+        
+        street_types = ['УЛ.', 'ПРОСП.', 'ПР.', 'ПЕР.', 'Ш.', 'НАБ.', 'Б-Р', 'БУЛЬВАР', 'ПЛ.', 'ПЛОЩАДЬ']
+        
+        if parts[0] in street_types:
+            street_type = parts[0]
+            name_only = ' '.join(parts[1:])
+            return f"{name_only}, {street_type}"
+        elif parts[-1] in street_types:
+            return street_name
+        else:
+            return street_name
